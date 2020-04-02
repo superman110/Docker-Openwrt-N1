04064c0b740a84c669e989821128df631ddcfae9
superman110 <superman110@gmail.com>
2020-04-02 23:30:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# firewall custom
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+# Debug kpr adapt aarch64
+sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
