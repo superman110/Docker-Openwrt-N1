79487a31b0940a6c2fc2e14d38237f8cb92ea772
superman110 <superman110@gmail.com>
2021-02-16 21:38:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,21 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package-temp
+mv -f package-temp/luci-app-openclash package/lean/
+rm -rf package-temp
+
+# Add luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/kenzok8/small.git package-temp/small
+cp -r package-temp/small/* package/lean/
+mv -f package-temp/lua-maxminddb package/lean/
+mv -f package-temp/luci-app-vssr package/lean/
+rm -rf package-temp
