c4e221feefdf58279f1bdb3871aadb6ad32cba0e
superman110 <superman110@gmail.com>
2021-02-23 23:42:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add luci-app-openclash
 git clone https://github.com/vernesong/OpenClash.git package-temp
