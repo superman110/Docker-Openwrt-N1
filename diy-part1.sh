7dc167ff1365f98354c919d0863262f52ee350dc
superman110 <superman110@gmail.com>
2022-07-04 22:06:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,16 +29,3 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git pull
 
-#修改晶晨宝盒默认配置
-
-# 1.Set the download repository of the OpenWrt files to your github.com
-sed -i "s|https.*/OpenWrt|https://github.com/superman110/Docker-Openwrt-N1|g" package/luci-app-amlogic/root/etc/config/amlogic
-
-# 2.Set the keywords of Tags in your github.com Releases
-#sed -i "s|ARMv8|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
-
-# 3.Set the suffix of the OPENWRT files in your github.com Releases
-#sed -i "s|.img.gz|_FOL+SFE.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
-
-# 4.Set the download path of the kernel in your github.com repository
-sed -i "s|opt/kernel|BuildARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
